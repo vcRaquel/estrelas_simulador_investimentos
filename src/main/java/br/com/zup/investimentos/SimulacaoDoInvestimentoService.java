fd7e1f2dafc46282c23aba304cbffa330e29d04c
Raquel Leão <vc.raquel@gmail.com>
2021-11-04 12:10:52 -0300

    calcular montante

--- src/main/java/br/com/zup/investimentos/SimulacaoDoInvestimentoService.java
+++ src/main/java/br/com/zup/investimentos/SimulacaoDoInvestimentoService.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SimulacaoDoInvestimentoService {
+
+    public double calcularMontante(double valorInvestido, int quantidadeMeses, TipodeRisco tipodeRisco){
+        double montante = valorInvestido * Math.pow((1 + tipodeRisco.getTaxaDeRetorno()), quantidadeMeses);
+        return montante;
+    }
+
+
+
 }
