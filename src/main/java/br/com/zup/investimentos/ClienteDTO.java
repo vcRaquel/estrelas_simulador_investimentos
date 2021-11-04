867690031a16f1ee1eaa640004423d4b472ec5e7
Raquel Leão <vc.raquel@gmail.com>
2021-11-04 11:17:08 -0300

    modelar classe ClienteDTO não finalizado

---
+++ src/main/java/br/com/zup/investimentos/ClienteDTO.java
@@ -0,0 +1,11 @@
+package br.com.zup.investimentos;
+
+public class ClienteDTO {
+    private String email;
+    private String nome;
+    private String cpf;
+    //atributos de SimulacaoDaAplicacao
+    private TipodeRisco risco;
+
+
+}
