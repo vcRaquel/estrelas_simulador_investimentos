6eed69e6ad9366f433fc07303c3a2680362aaab6
Raquel Leão <vc.raquel@gmail.com>
2021-11-04 12:58:49 -0300

    calcular multiplicador de capital para cálculo do montante

--- src/main/java/br/com/zup/investimentos/SimulacaoDoInvestimentoService.java
+++ src/main/java/br/com/zup/investimentos/SimulacaoDoInvestimentoService.java
@@ -5,11 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class SimulacaoDoInvestimentoService {
 
+   public double calcularMultiplicadorDeCapital(TipodeRisco tipoDeRisco, int quantidadeMeses){
+       double multiplicadorDeCapital = Math.pow((1 + tipoDeRisco.getTaxaDeRetorno()), quantidadeMeses);
+       return multiplicadorDeCapital;
+   }
+
     public double calcularMontante(double valorInvestido, int quantidadeMeses, TipodeRisco tipodeRisco){
-        double montante = valorInvestido * Math.pow((1 + tipodeRisco.getTaxaDeRetorno()), quantidadeMeses);
+        double montante = valorInvestido * calcularMultiplicadorDeCapital(tipodeRisco, quantidadeMeses);
         return montante;
     }
 
-
+    public double calcularLucro(double valorInvestido, double motante){
+        double lucro = motante - valorInvestido;
+        return lucro;
+    }
 
 }
