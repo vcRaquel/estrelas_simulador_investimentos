d3f1ed22000dd76329be901fd0d5fd43d1749b3f
Raquel Leão <vc.raquel@gmail.com>
2021-11-04 10:55:52 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/investimentos/InvestimentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.investimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/investimentos/InvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
