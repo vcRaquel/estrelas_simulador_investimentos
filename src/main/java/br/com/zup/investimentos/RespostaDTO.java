70b4daee8de28cab38b64220a70f5d92c978b159
Raquel Leão <vc.raquel@gmail.com>
2021-11-04 14:28:05 -0300

    criar classe RespostaDTO

---
+++ src/main/java/br/com/zup/investimentos/RespostaDTO.java
@@ -0,0 +1,37 @@
+package br.com.zup.investimentos;
+
+public class RespostaDTO {
+    private double valorInvestido;
+    private double valorTotalDoLucro;
+    private double valorTotal;
+
+    public RespostaDTO(double valorInvestido, double valorTotalDoLucro, double valorTotal) {
+        this.valorInvestido = valorInvestido;
+        this.valorTotalDoLucro = valorTotalDoLucro;
+        this.valorTotal = valorTotal;
+    }
+
+    public double getValorInvestido() {
+        return valorInvestido;
+    }
+
+    public void setValorInvestido(double valorInvestido) {
+        this.valorInvestido = valorInvestido;
+    }
+
+    public double getValorTotalDoLucro() {
+        return valorTotalDoLucro;
+    }
+
+    public void setValorTotalDoLucro(double valorTotalDoLucro) {
+        this.valorTotalDoLucro = valorTotalDoLucro;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
